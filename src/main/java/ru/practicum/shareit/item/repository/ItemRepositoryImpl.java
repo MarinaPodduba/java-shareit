aa5602b0e965f867bf50b5d6c63a840c52b521b0
MarinaPodduba <m.shahova18@yandex.ru>
2023-05-18 01:33:38 -0300

    Checkstyle

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepositoryImpl.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.repository;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.exception.NotFoundException;
